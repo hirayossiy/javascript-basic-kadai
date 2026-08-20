fb710e920fca686aa473f1bd6e0bd6d34fcae06a
hirayossiy <sss.hiramori@gmail.com>
2026-08-20 18:00:28 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+
+// btnにbtnを取得
+const btn=document.getElementById('btn');
+const nextText=document.getElementById('text');
+
+btn.addEventListener('click',()=>{
+    nextText.textContent="ボタンをクリックしました";
+})
+
+
